3fcc6cb6da79f6b3b543cec898fb9796f86c8449
Hack Reactor Students <accounts@hackreactor.com>
2014-10-09 10:37:14 -0700

    Deploying to prod with mongo in env variable

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var app = require('./server-config.js');
 
 var port = process.env.PORT || 4568;
-
+var db = process.env.MONGODB || mongodb://localhost/shortly
 app.listen(port);
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/shortly');
+mongoose.connect(db);
 
 console.log('Server now listening on port ' + port);
