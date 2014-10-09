485887b66c56906e13f8924ebb6f61dd4da32365
Hack Reactor Students <accounts@hackreactor.com>
2014-10-09 10:56:55 -0700

    fix length issue

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var port = process.env.PORT || 4568;
 var db = process.env.MONGODB || 'mongodb://localhost/shortly'
 app.listen(port);
 var mongoose = require('mongoose');
-
 mongoose.connect(db);
 
 console.log('Server now listening on port ' + port);
